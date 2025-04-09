fcef517898b55c508c07a78b2ca31ee373eb43b8
user <nurdin.ametov@alatoo.edu.kg>
2025-04-09 12:44:55 +0600

    1

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Tourist Destination Catalog
+        // Catalogs tourist destinations, providing information about attractions, accommodations and activities for travelers
+
+
+    }
+}
